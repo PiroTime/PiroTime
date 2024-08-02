bc6be6258eebc0344f02c27650965cc5c0c28a18
leesuyong849 <toosee1005@cau.ac.kr>
2024-08-02 20:27:03 +0900

    [coffeechat part 2 작성 중

--- coffeechat/views.py
+++ coffeechat/views.py
@@ -1,3 +1,5 @@
+from django.core.mail import send_mail
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 
 from coffeechat.models import CoffeeChat
@@ -16,3 +18,33 @@ def add_bookmark_coffee(request, pk):
 
 def send_email_coffee(request, pk):
     post = get_object_or_404(CoffeeChat, pk=pk)
+
+    if post.count > 10:
+        return redirect('views.main')       #detail 페이지로 던져야
+    post.count += 1
+    post.status = 'WAITING'
+
+
+def cor_mail(request, pk):
+    receiver = get_object_or_404(CoffeeChat, pk=pk).writer
+    sender = request.user
+
+
+
+    subject, message, from_email, recipient_list = generate_email_content(sender, receiver)
+    send_mail(
+        subject,
+        message,
+        from_email,
+        recipient_list,
+        fail_silently=False,
+    )
+    return HttpResponse('Email sent successfully')
+
+
+def generate_email_content(sender, receiver):
+    subject = "CoffeeChat 친청이 왔습니다."
+    message = f"{sender.username}님으로 부터 협력 제안이 왔습니다. PiroTime에 접속해서 내용을 확인하세요!"
+    from_email = 'pirotimeofficial@gmail.com'
+    recipient_list = [receiver.email]
+    return subject, message, from_email, recipient_list
