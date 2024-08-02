620139f91b5a429b3efaecf84504374dba8d8816
leesuyong849 <toosee1005@cau.ac.kr>
2024-08-02 18:31:03 +0900

    [UPLOAD] 로직 작성 중

--- coffeechat/views.py
+++ coffeechat/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from coffeechat.models import CoffeeChat
+
 
 def main(request):
     return render(request, '/coffeechat/main.html')
+
+def add_bookmark_coffee(request, pk):
+    post = get_object_or_404(CoffeeChat, pk=pk)
+    if post.bookmarks.filter(id=request.user.id).exists():
+        post.bookmarks.remove(request.user.id)
+    else:
+        post.bookmarks.add(request.user)
+    return redirect('views.main')
+
+def send_email_coffee(request, pk):
+    post = get_object_or_404(CoffeeChat, pk=pk)
