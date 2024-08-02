4af714833e5c277c20b97006991bcfa736757edf
yeonjin yi <c2yjcs03@sookmyung.ac.kr>
2024-08-02 14:03:45 +0900

    [FEAT]coffeechat model, urls, html 등 기본 세팅 완료

---
+++ coffeechat/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from accounts.models import CustomUser
+# Create your models here.
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+class CoffeeChat(models.Model):
+    STATUS_CHOICES = [
+        ('WAITING','수락대기중'),
+        ('COMPLETED','수락'),
+        ('LIMITED','최대요청횟수초과'),
+        ('Private','비공개')
+    ]
+    username = models.ForeignKey(CustomUser, related_name='username', on_delete=models.CASCADE) #커피챗 요청자
+    receiver = models.ForeignKey(CustomUser, related_name='receiver', on_delete=models.CASCADE) #커피챗 요청받은 사람
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, defalut='WAITING') #커피챗 상태
+    job = models.CharField(max_length=10, null=False) #직업
+    created_at = models.DateTimeField(auto_now_add=True) #요청시간
+    hashtags = models.ManyToManyField(Hashtag, related_name='coffeechats') #해시태그
+    content = models.TextField(null=True, blank=True) #자기소개
+    count = models.IntegerField() #요청 수
+    
