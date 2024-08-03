317e3cd621c5776263c0050b1a6d3bc93dcf87ee
yeonjin yi <c2yjcs03@sookmyung.ac.kr>
2024-08-03 18:51:15 +0900

    [feat]coffeechat 프로필 생성, detail, 수정/삭제 구현

--- coffeechat/models.py
+++ coffeechat/models.py
@@ -5,6 +5,9 @@ from accounts.models import CustomUser
 class Hashtag(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name  # 해시태그의 이름을 반환
+
 class CoffeeChat(models.Model):
     STATUS_CHOICES = [
         ('WAITING','수락대기중'),
@@ -12,12 +15,12 @@ class CoffeeChat(models.Model):
         ('LIMITED','최대요청횟수초과'),
         ('Private','비공개')
     ]
-    username = models.ForeignKey(CustomUser, related_name='username', on_delete=models.CASCADE) #커피챗 요청자
-    receiver = models.ForeignKey(CustomUser, related_name='receiver', on_delete=models.CASCADE) #커피챗 요청받은 사람
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, defalut='WAITING') #커피챗 상태
+    username = models.ForeignKey(CustomUser, related_name='sent_coffeechats', on_delete=models.CASCADE, null=True, blank=True) #커피챗 요청자
+    receiver = models.ForeignKey(CustomUser, related_name='received_coffeechats', on_delete=models.CASCADE) #커피챗 요청받은 사람
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='WAITING') #커피챗 상태
     job = models.CharField(max_length=10, null=False) #직업
     created_at = models.DateTimeField(auto_now_add=True) #요청시간
     hashtags = models.ManyToManyField(Hashtag, related_name='coffeechats') #해시태그
     content = models.TextField(null=True, blank=True) #자기소개
-    count = models.IntegerField() #요청 수
+    count = models.IntegerField(default=0) #요청 수
     
--- coffeechat/urls.py
+++ coffeechat/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='main'),
+    path('', views.main, name='coffeechat_main'),
+    path('list/', views.list, name='coffeechat_list'),
+    path('create/', views.create, name='coffeechat_create'),
+    path('detail/<int:pk>/', views.detail, name='coffeechat_detail'),
+    path('update/<int:pk>/', views.update, name='coffeechat_update'),
+    path('delete/<int:pk>/', views.delete, name='coffeechat_delete'),
 ]
