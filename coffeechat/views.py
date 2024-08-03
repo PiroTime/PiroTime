ff7b9299771113c53d8e8deb991ebf9f25a681b7
leesuyong849 <toosee1005@cau.ac.kr>
2024-08-03 21:36:08 +0900

    [UPDATE] email func

--- coffeechat/views.py
+++ coffeechat/views.py
@@ -1,6 +1,8 @@
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, send_mail
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 from coffeechat.models import CoffeeChat
 
@@ -19,24 +21,33 @@ def add_bookmark_coffee(request, pk):
 def send_email_coffee(request, pk):
     post = get_object_or_404(CoffeeChat, pk=pk)
 
-    if post.count > 10:
-        return redirect('views.main')       #detail 페이지로 던져야
     post.count += 1
     post.status = 'WAITING'
 
 
-def cor_mail(request, pk):
-    receiver = get_object_or_404(CoffeeChat, pk=pk).username
-    sender = request.user
-
-    subject, message, from_email, recipient_list = generate_email_content(sender, receiver)
-    EmailMessage(subject=subject, from_email=from_email, to=recipient_list, body=message).send()
-    return HttpResponse('Email sent successfully')
-
 
 def generate_email_content(sender, receiver):
-    subject = "CoffeeChat 친청이 왔습니다."
-    message = f"{sender.username}님으로 부터 협력 제안이 왔습니다. PiroTime에 접속해서 내용을 확인하세요!"
+    subject = "PiroTime: 커피챗 제안이 왔습니다!"
+    message = f"{sender.username}님으로 부터 커피챗 제안이 왔습니다. PiroTime에 접속해서 내용을 확인하세요!"
     from_email = 'pirotimeofficial@gmail.com'
     recipient_list = [receiver.email]
     return subject, message, from_email, recipient_list
+
+def cor_mail(request, pk):
+    receiver = get_object_or_404(CoffeeChat, pk=pk).writer
+    sender = request.user
+    subject, message, from_email, recipient_list = generate_email_content(sender, receiver)
+
+    html_message = render_to_string(
+        "corboard/message.html",
+        {"sender": sender.username, "receiver": receiver.username},
+    )
+    plain_message = strip_tags(html_message)
+    send_mail(
+        subject,
+        plain_message,
+        from_email,
+        recipient_list,
+        html_message=html_message,
+    )
+    return HttpResponse('Email sent successfully')
