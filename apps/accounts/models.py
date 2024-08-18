34bde8b23bf8edee2052239b34513e51dd8a099d
leesuyong849 <toosee1005@cau.ac.kr>
2024-08-18 18:51:41 +0900

    [ADD] 동의 하면 저장하는 model 추가

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
+
+
 
 class CustomUser(AbstractUser):
     nickname = models.CharField(max_length=30, blank=True)
